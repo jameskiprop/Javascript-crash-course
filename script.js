578cdead7284acd034c5ab9f79a51e3695f2677b
jameskiprop <jameskiprop01@gmail.com>
2024-06-09 22:23:46 +0300

    Js crash

---
+++ script.js
@@ -0,0 +1,31 @@
+// script numbers, Boolean, Null, Undefined,
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+//concatenation
+console.log("My name is " + name + " and I am " + age);
+
+// template string
+const hello = "My name is ${name} and I am ${age}";
+
+console.log(hello);
+
+const s = "Hello world!";
+
+console.log(s.toLowerCase());
+
+console.log(s.substring(0, 5).toUpperCase());
+
+console.log(s.split(""));
+
+//Arrays - variables that hold multiple values
+
+const fruits = ["apples", "pears", "oranges", "dates", "pumpkins"];
+
+console.log(fruits);
