69b1ff3c6998e875428d0e8546febdc35d59d546
jameskiprop <jameskiprop01@gmail.com>
2024-06-12 22:26:28 +0300

    progress on the js crash course

--- script.js
+++ script.js
@@ -36,114 +36,95 @@ fruits.unshift("strawberries"); // adds strawberries to the beginning of the arr
 
 fruits.pop(); //pops the last element of the array off
 
-console.log(Array.isArray(fruits)); //checking if the element in the array is truly an array
+console.log(Array.isArray(fruits)); //checking if the element in the array is truly an array.
 
-console.log(fruits.indexOf("oranges"));
+console.log(fruits.indexOf("oranges")); //tells us the index in which the element is located in the array.
 
-console.log(fruits);
+console.log(fruits); //code compiler
 
-// //
-// const person = {
-//   firstName: "James",
-//   lastName: "Kiprop",
-//   age: 23,
-//   hobbies: ["swimming", "hiking", "travelling", "Gaming"],
-//   address: {
-//     street: "50 main st",
-//     city: "Boston",
-//     state: "MA",
-//   },
-// };
+//
+const person = {
+  firstName: "James",
+  lastName: "Kiprop",
+  age: 23,
+  hobbies: ["swimming", "hiking", "travelling", "Gaming"],
+  address: {
+    street: "50 main st",
+    city: "Boston",
+    state: "MA",
+  },
+};
+person.email = "jameskiprop01@gmail.com"; //Directly adding properties to the attributes
+// console.log(person.hobbies[1]); //calling the element on the first index position of the hobbies array
+console.log(person);
 
-// person.email = "jameskiprop01@gmail.com"; //Directly adding properties to the email
-
-// console.log(person);
-
-//Arrays of Objects
-
+//creating arrays of objects
 const todos = [
   {
     id: 1,
     text: "Take out trash",
-    isCoolompleted: true,
+    isCompleted: true,
   },
+
   {
     id: 2,
-    text: "Go and meet with some senior developers",
-    isCoolompleted: true,
+    text: "Spend the weekend with my shawrie",
+    isCompleted: true,
   },
+
   {
     id: 3,
-    text: "finish up on building my API",
-    isCoolompleted: false,
+    text: "Hiking on Ngong hills on the last weekend of the month",
+    isCompleted: false,
   },
 ];
-
 console.log(todos[1].text);
 
-//for
+// const todoJSON = JSON.stringify(todos);
+// console.log(todoJSON);
 
-for (let i = 0; i <= 10; i++);
-{
-  console.log("For Loop Number: ${i}");
+//For
+for (let i = 0; i <= 10; i++) {
+  console.log(`For Loop Number: ${i}`);
 }
 
 //while
+let i = 0;
+while (i < 10) {
+  console.log(`while loop number: ${i}`);
+  i++;
+}
 
-for (let i = 0; i < 10; i++);
+//Looping through Arrays
+for (let i = 0; i < todos.length; i++);
 {
-  console.log("While Loop Number: ${i}");
+  console.log(`For loop Number:`);
 }
 
-//for
-
-// todos.forEach(function (todo) {
-//   console.log(todo.text);
-// });
-
-// const todoCompleted = todos
-//   .filter(function (todo) {
-//     return todo.isCompleted == true;
-//   })
-//   .map(function (todo) {
-//     return todo.text;
-//   });
-
-// console.log(todoCompleted);
-
-const x = 16;
-
-const color = "green";
-
-console.log(color);
-
-switch (color) {
-  case "red":
-    console.log("color is red");
-    break;
-  case "blue":
-    console.log("color is blue");
-    break;
-  default:
-    console.log("color is not red or blue");
+for (let todo of todos) {
+  console.log(todo.isCompleted);
 }
-//functions
-function addNums(num1 = 1, num2 = 1) {
-  console.log(num1 + num2);
-}
-addNums();
-//constructor function
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName; // this points to the newly created object
-  this.lastname = lastName;
-  this.dob = new Date(dob);
-  this.getBirthYear = function () {
-    return this.dob;
-  };
-}
+//forEach, map, filter
 
-//instantiate object
-const person1 = new Person("John", "Doe", "4-3-1980"); //"new" initialized the new object that will be created
-const person2 = new Person("Mary", "Jane", "6-5-1989");
+// forEach
+todos.forEach(function (todo) {
+  console.log(todo.id);
+});
 
-console.log(person2);
+//map
+const todoText = todos.map(function (todo) {
+  return todo.text;
+});
+
+console.log(todoText);
+
+//filter
+const todoCompleted = todos
+  .filter(function (todo) {
+    return todo.isCompleted === true;
+  })
+  .map(function (todo) {
+    return todo.text;
+  });
+
+console.log(todoCompleted);
